75b4d607eb8cccdb176db2e5f8d52f95e8518bc1
Vince Cfl <chenfeilong@tf.com>
2023-04-17 00:35:58 +0800

    update

--- errno/code.go
+++ errno/code.go
@@ -1,10 +1,10 @@
 package errno
 
 var (
-	OK                = &Errno{Code: 0, Msg: "success"}
-	ParamError        = &Errno{Code: 1, Msg: "param error"}
-	AuthError         = &Errno{Code: 2, Msg: "auth error"}
-	AccountOrPwdError = &Errno{Code: 3, Msg: "account or pwd error"}
-	HandleError       = &Errno{Code: 4, Msg: "handle error"}
-	OtherError        = &Errno{Code: 5, Msg: "other error"}
+	OK                = &Errno{Code: 0, Msg: "成功"}
+	ParamError        = &Errno{Code: 1, Msg: "请求参数错误"}
+	AuthError         = &Errno{Code: 2, Msg: "登陆状态已失效"}
+	AccountOrPwdError = &Errno{Code: 3, Msg: "账户或密码错误"}
+	HandleError       = &Errno{Code: 4, Msg: "处理错误"}
+	OtherError        = &Errno{Code: 5, Msg: "其他错误"}
 )
