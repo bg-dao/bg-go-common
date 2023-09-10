65975ec9c0841d78ece7bc97eee9efa043748668
bg-vc <vincecfl@gmail.com>
2023-09-10 21:23:27 +0800

    update

--- errno/code.go
+++ errno/code.go
@@ -5,7 +5,7 @@ var (
 	ParamError            = &Errno{Code: 1, Msg: "请求参数错误"}
 	AuthError             = &Errno{Code: 2, Msg: "登陆状态已失效"}
 	AccountOrPwdError     = &Errno{Code: 3, Msg: "账户或密码错误"}
-	HandleError           = &Errno{Code: 4, Msg: "处理错误"}
+	HandleError           = &Errno{Code: 4, Msg: "服务处理错误"}
 	ChatContextBizIdError = &Errno{Code: 5, Msg: "对话上下文业务ID已时效"}
 	OtherError            = &Errno{Code: 6, Msg: "其他错误"}
 )
