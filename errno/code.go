b43b574c613452aa0c412c7bf4b34884ee553785
bg-vc <vincecfl@gmail.com>
2023-09-10 21:21:06 +0800

    update

--- errno/code.go
+++ errno/code.go
@@ -1,10 +1,11 @@
 package errno
 
 var (
-	OK                = &Errno{Code: 0, Msg: "成功"}
-	ParamError        = &Errno{Code: 1, Msg: "请求参数错误"}
-	AuthError         = &Errno{Code: 2, Msg: "登陆状态已失效"}
-	AccountOrPwdError = &Errno{Code: 3, Msg: "账户或密码错误"}
-	HandleError       = &Errno{Code: 4, Msg: "处理错误"}
-	OtherError        = &Errno{Code: 5, Msg: "其他错误"}
+	OK                    = &Errno{Code: 0, Msg: "成功"}
+	ParamError            = &Errno{Code: 1, Msg: "请求参数错误"}
+	AuthError             = &Errno{Code: 2, Msg: "登陆状态已失效"}
+	AccountOrPwdError     = &Errno{Code: 3, Msg: "账户或密码错误"}
+	HandleError           = &Errno{Code: 4, Msg: "处理错误"}
+	ChatContextBizIdError = &Errno{Code: 5, Msg: "对话上下文业务ID已时效"}
+	OtherError            = &Errno{Code: 6, Msg: "其他错误"}
 )
